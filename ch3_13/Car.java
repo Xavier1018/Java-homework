4582fbdf3906f80547f3a93c9354f83bf04f13ae
Xavier <163835737+Xavier1018@users.noreply.github.com>
2024-11-10 21:03:43 +0800

    Update Car.java

--- ch3_13/Car.java
+++ ch3_13/Car.java
@@ -1,53 +1,49 @@
 package ch3_13;
 
 public class Car {
-    // Instance variables
     private String model;
     private String year;
     private double price;
 
-    // Constructor to initialize the variables
+    // Constructor
     public Car(String model, String year, double price) {
-        this.model = model;
-        this.year = year;
-        if (price > 0) {
-            this.price = price;
-        } else {
-            this.price = 0;
-        }
+        setModel(model);
+        setYear(year);
+        setPrice(price);
     }
 
-    // Getter and setter for model
-    public String getModel() {
-        return model;
-    }
-
+    // Setters
     public void setModel(String model) {
         this.model = model;
-    }
-
-    // Getter and setter for year
-    public String getYear() {
-        return year;
     }
 
     public void setYear(String year) {
         this.year = year;
     }
 
-    // Getter and setter for price
+    public void setPrice(double price) {
+        if (price > 0) {
+            this.price = price;
+        } else {
+            this.price = 0; // Set to 0 if the price is invalid
+        }
+    }
+
+    // Getters
+    public String getModel() {
+        return model;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
     public double getPrice() {
         return price;
     }
 
-    public void setPrice(double price) {
-        if (price > 0) {
-            this.price = price;
-        }
-    }
-
     // Method to apply discount
-    public void applyDiscount(double discountPercentage) {
+    public void discount(double discountPercentage) {
         if (discountPercentage > 0 && discountPercentage < 100) {
             price -= price * (discountPercentage / 100);
         }
@@ -55,21 +51,17 @@ public class Car {
 
     // Main method to test the Car class
     public static void main(String[] args) {
-        // Create two Car objects
-        Car car1 = new Car("Toyota Corolla", "2022", 20000);
-        Car car2 = new Car("Honda Civic", "2021", 22000);
+        Car c1 = new Car("Toyota", "2000", 20000);
+        Car c2 = new Car("Benz", "1999", 25000);
 
-        // Display each Car's price before applying discount
-        System.out.println("Car 1 price before discount: $" + car1.getPrice());
-        System.out.println("Car 2 price before discount: $" + car2.getPrice());
+        System.out.println("Original price of car 1: " + c1.getPrice());
+        System.out.println("Original price of car 2: " + c2.getPrice());
 
         // Apply discounts
-        car1.applyDiscount(5);  // 5% discount on car1
-        car2.applyDiscount(7);  // 7% discount on car2
+        c1.discount(5);
+        c2.discount(7);
 
-        // Display each Car's price after applying discount
-        System.out.println("Car 1 price after 5% discount: $" + car1.getPrice());
-        System.out.println("Car 2 price after 7% discount: $" + car2.getPrice());
+        System.out.println("After discount, car 1 price: " + c1.getPrice());
+        System.out.println("After discount, car 2 price: " + c2.getPrice());
     }
 }
-
