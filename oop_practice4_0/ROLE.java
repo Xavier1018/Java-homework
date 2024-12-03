249648f94f74e1d2be76a1f1e7e9d48d754ee95c
Xavier <163835737+Xavier1018@users.noreply.github.com>
2024-12-03 11:15:21 +0800

    Update ROLE.java

--- oop_practice4_0/ROLE.java
+++ oop_practice4_0/ROLE.java
@@ -1,36 +1,63 @@
 package oop_practice4_0;
 
-//Parent class for all character roles
-public class ROLE {
- protected String name;
- protected int life;
- protected int magic;
+public abstract class ROLE {
+    protected String name;
+    protected int life;
+    protected int magic;
 
- // Constructor with parameters for initializing name, life, and magic
- public ROLE(String name, int life, int magic) {
-     this.name = name;
-     this.life = life;
-     this.magic = magic;
- }
+    // Constructor to initialize name, life, and magic values
+    public ROLE(String name, int life, int magic) {
+        this.name = name;
+        this.life = life;
+        this.magic = magic;
+    }
 
- // Getter method for name
- public String getName() {
-     return name;
- }
+    // Getter for life
+    public int getLife() {
+        return life;
+    }
 
- // Getter method for life
- public int getLife() {
-     return life;
- }
+    // Setter for life
+    public void setLife(int life) {
+        this.life = life;
+    }
 
- // Getter method for magic
- public int getMagic() {
-     return magic;
- }
+    // Getter for magic
+    public int getMagic() {
+        return magic;
+    }
 
- // Overriding toString() method for printing status
- @Override
- public String toString() {
-     return "Name: " + name + ", Life: " + life + ", Magic: " + magic;
- }
+    // Setter for magic
+    public void setMagic(int magic) {
+        this.magic = magic;
+    }
+
+    // Getter for name
+    public String getName() {
+        return name;
+    }
+
+    // Abstract method to attack
+    public abstract void attack(ROLE target);
+
+    // Method to check if the character is alive
+    public boolean isAlive() {
+        return life > 0;
+    }
+
+    // Method to reduce life
+    public void reduceLife(int amount) {
+        life -= amount;
+    }
+
+    // Method to reduce magic
+    public void reduceMagic(int amount) {
+        magic -= amount;
+    }
+
+    // Method to recover life (used by LifeRecoverable interface)
+    public abstract double recoverLife();
+
+    // Method to recover magic (used by MagicRecoverable interface)
+    public abstract double recoverMagic();
 }
