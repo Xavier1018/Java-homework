7c649b3e43730b94289e9a075a58197f76c44b9d
Xavier <163835737+Xavier1018@users.noreply.github.com>
2024-12-03 11:14:26 +0800

    Update LifeRecoverable.java

--- oop_practice4_0/LifeRecoverable.java
+++ oop_practice4_0/LifeRecoverable.java
@@ -2,9 +2,10 @@ package oop_practice4_0;
 
 //Interface for life recovery
 public interface LifeRecoverable {
- // Constant for life recovery rate
- public static final int LIFERATE = 10;
- 
- // Abstract method for recovering life
- public abstract double recoverLife();
+//Constant for life recovery rate
+public static final int LIFERATE = 10;
+
+//Abstract method for recovering life
+public abstract double recoverLife();
 }
+
