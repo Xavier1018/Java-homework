f2851c5dc7b7795a677c457da07efc9f64c5f118
Xavier <163835737+Xavier1018@users.noreply.github.com>
2024-12-03 11:11:13 +0800

    Update ROLE.java

--- oop_practice3/ROLE.java
+++ oop_practice3/ROLE.java
@@ -1,81 +1,82 @@
 package oop_practice3;
+import java.util.Random;
 
-public class ROLE {
+public abstract class ROLE {
     private String name;
     private int life;
     private int magic;
 
-    // Constructor to initialize the character's name, life, and magic points.
     public ROLE(String name, int life, int magic) {
         this.name = name;
         this.life = life;
         this.magic = magic;
     }
 
-    // Getter method for the character's name.
+    public abstract void NewMoon(ROLE target);
+    public abstract void SmallFire(ROLE target);
+
     public String getName() {
         return name;
     }
 
-    // Getter method for the character's current life points.
     public int getLife() {
         return life;
     }
 
-    // Getter method for the character's current magic points.
     public int getMagic() {
         return magic;
     }
 
-    // The Drink method allows a character to drink a drug (either RedDrug or BlueDrug).
-    // It updates the character's life or magic based on the drug's size.
-    public void Drink(Drug D) {
-        if (D instanceof RedDrug) { // If the drug is of type RedDrug
-            RedDrug redDrug = (RedDrug) D;
-            switch (redDrug.getSize()) {
-                case "Large":
-                    life += 120; // Increase life by 120 for Large RedDrug
-                    break;
-                case "Medium":
-                    life += 80; // Increase life by 80 for Medium RedDrug
-                    break;
-                case "Small":
-                    life += 50; // Increase life by 50 for Small RedDrug
-                    break;
-            }
-        } else if (D instanceof BlueDrug) { // If the drug is of type BlueDrug
-            BlueDrug blueDrug = (BlueDrug) D;
-            switch (blueDrug.getSize()) {
-                case "Large":
-                    magic += 100; // Increase magic by 100 for Large BlueDrug
-                    break;
-                case "Medium":
-                    magic += 60; // Increase magic by 60 for Medium BlueDrug
-                    break;
-                case "Small":
-                    magic += 30; // Increase magic by 30 for Small BlueDrug
-                    break;
-            }
-        }
-
-        // Ensures that life does not exceed 100 and magic does not exceed 70
-        if (life > 100) {
-            life = 100; // Cap life at 100
-        }
-
-        if (magic > 70) {
-            magic = 70; // Cap magic at 70
-        }
-
-        // Output the action of drinking the drug
-        System.out.println(name + " drank a " + D.getSize() + " bottle.");
+    public void reduceLife(int amount) {
+        life = Math.max(0, life - amount);
     }
 
-    // Overriding toString() to print the current status of the character
+    public void reduceMagic(int amount) {
+        magic = Math.max(0, magic - amount);
+    }
+
+    public boolean isAlive() {
+        return life > 0;
+    }
+
+    public void autoDrink() {
+        Random random = new Random();
+        if (getLife() < 100) {
+            int chance = random.nextInt(100);
+            if (chance < 10) {
+                System.out.println(getName() + " drinks a large red drug.");
+                life += 120;
+            } else if (chance < 40) {
+                System.out.println(getName() + " drinks a medium red drug.");
+                life += 80;
+            } else if (chance < 80) {
+                System.out.println(getName() + " drinks a small red drug.");
+                life += 50;
+            } else {
+                System.out.println(getName() + " couldn't find a red drug to drink.");
+            }
+        }
+
+        if (getMagic() < 70) {
+            int chance = random.nextInt(100);
+            if (chance < 10) {
+                System.out.println(getName() + " drinks a large blue drug.");
+                magic += 100;
+            } else if (chance < 40) {
+                System.out.println(getName() + " drinks a medium blue drug.");
+                magic += 60;
+            } else if (chance < 80) {
+                System.out.println(getName() + " drinks a small blue drug.");
+                magic += 30;
+            } else {
+                System.out.println(getName() + " couldn't find a blue drug to drink.");
+            }
+        }
+    }
+
     @Override
     public String toString() {
         return "Name: " + name + ", Life: " + life + ", Magic: " + magic;
     }
 }
-
 
