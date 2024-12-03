1c7d440289fd8c93274e0aa481e687a3b4bcc2a9
Xavier <163835737+Xavier1018@users.noreply.github.com>
2024-12-03 11:14:05 +0800

    Update BattleGame.java

--- oop_practice4_0/BattleGame.java
+++ oop_practice4_0/BattleGame.java
@@ -1,58 +1,19 @@
 package oop_practice4_0;
-
-import java.util.Random;
-
 public class BattleGame {
     public static void main(String[] args) {
-        // Create characters using default constructors
-        Warrior warrior = new Warrior();
-        Witch witch = new Witch();
-        Priest priest = new Priest();
+        Warrior warrior = new Warrior("Warrior");
+        Witch witch = new Witch("Witch");
+        Priest priest = new Priest("Priest");
 
-        Random random = new Random();
+        // Example of polymorphism: calling the attack methods
+        warrior.attack(witch);   // Warrior attacks Witch
+        witch.attack(priest);    // Witch attacks Priest
+        priest.attack(warrior);  // Priest attacks Warrior
 
-        // Simulate the battle
-        while (warrior.getLife() > 0 && witch.getLife() > 0 && priest.getLife() > 0) {
-            // Warrior attacks Witch
-            int chance = random.nextInt(100);
-            if (warrior.getMagic() >= 10 && chance < 50) {
-                warrior.NewMoon(witch); // Warrior attacks witch
-            }
-
-            // Witch attacks Priest
-            chance = random.nextInt(100);
-            if (witch.getMagic() >= 25 && chance < 50) {
-                witch.SmallFire(priest); // Witch attacks priest
-            }
-
-            // Priest attacks Warrior
-            chance = random.nextInt(100);
-            if (priest.getMagic() >= 20 && chance < 50) {
-                priest.Dark(warrior); // Priest attacks warrior
-            }
-
-            // Automatic life and magic recovery
-            warrior.recoverLife();
-            witch.recoverMagic();
-
-            // Print the current status of each character
-            System.out.println("Current Status: ");
-            System.out.println(warrior);
-            System.out.println(witch);
-            System.out.println(priest);
-            System.out.println();
-
-            // Check if all characters are dead
-            if (warrior.getLife() <= 0 || witch.getLife() <= 0 || priest.getLife() <= 0) {
-                break;
-            }
-        }
-
-        // Print the final status after the battle
-        System.out.println("Final Statuses: ");
-        System.out.println(warrior);
-        System.out.println(witch);
-        System.out.println(priest);
+        // Print final statuses
+        System.out.println("\nFinal Statuses:");
+        System.out.println(warrior.name + " - Life: " + warrior.life + ", Magic: " + warrior.magic);
+        System.out.println(witch.name + " - Life: " + witch.life + ", Magic: " + witch.magic);
+        System.out.println(priest.name + " - Life: " + priest.life + ", Magic: " + priest.magic);
     }
 }
-
