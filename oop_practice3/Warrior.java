2a5ce755aa154a4121dadde609f4fb017221524a
Xavier <163835737+Xavier1018@users.noreply.github.com>
2024-12-03 11:34:54 +0800

    Update Warrior.java

--- oop_practice3/Warrior.java
+++ oop_practice3/Warrior.java
@@ -1,88 +1,39 @@
 package oop_practice3;
-import java.util.Random;
 
-public abstract class ROLE {
-    private String name;
-    private int life;
-    private int magic;
 
-    public ROLE(String name, int life, int magic) {
-        this.name = name;
-        this.life = life;
-        this.magic = magic;
+public class Warrior extends ROLE {
+    public Warrior(String name) {
+        super(name, 400, 100); 
     }
+
+    public Warrior() {
+        super("unknown", 400, 100); 
+    }
+
     
-    public ROLE() {
-    	this.name="";
-    	this.life=0;
-    	this.magic=0;
-    }
-
-
-
-    public String getName() {
-        return name;
-    }
-
-    public int getLife() {
-        return life;
-    }
-
-    public int getMagic() {
-        return magic;
-    }
-
-    public void reduceLife(int amount) {
-        life = Math.max(0, life - amount);
-    }
-
-    public void reduceMagic(int amount) {
-        magic = Math.max(0, magic - amount);
-    }
-
-    public boolean isAlive() {
-        return life > 0;
-    }
-
-    public void autoDrink() {
-        Random random = new Random();
-        if (getLife() < 100) {
-            int chance = random.nextInt(100);
-            if (chance < 10) {
-                System.out.println(getName() + " drinks a large red drug.");
-                life += 120;
-            } else if (chance < 40) {
-                System.out.println(getName() + " drinks a medium red drug.");
-                life += 80;
-            } else if (chance < 80) {
-                System.out.println(getName() + " drinks a small red drug.");
-                life += 50;
-            } else {
-                System.out.println(getName() + " couldn't find a red drug to drink.");
-            }
+    public void NewMoon(ROLE target) {
+        if (target == null) {
+            System.out.println("Target is null. No action performed.");
+            return;
         }
 
-        if (getMagic() < 70) {
-            int chance = random.nextInt(100);
-            if (chance < 10) {
-                System.out.println(getName() + " drinks a large blue drug.");
-                magic += 100;
-            } else if (chance < 40) {
-                System.out.println(getName() + " drinks a medium blue drug.");
-                magic += 60;
-            } else if (chance < 80) {
-                System.out.println(getName() + " drinks a small blue drug.");
-                magic += 30;
-            } else {
-                System.out.println(getName() + " couldn't find a blue drug to drink.");
-            }
+        if (getMagic() < 10) {
+            System.out.println(getName() + " does not have enough magic to attack.");
+            return;
+        }
+
+        reduceMagic(10); 
+
+        
+        int damage = (target instanceof Witch) ? 40 : 25;
+        target.reduceLife(damage);
+
+        System.out.println(getName() + " attacks " + target.getName() + " with New Moon Sword.");
+        if (!target.isAlive()) {
+            System.out.println(target.getName() + " has died.");
         }
     }
 
-    @Override
-    public String toString() {
-        return "Name: " + name + ", Life: " + life + ", Magic: " + magic;
-    }
 }
 
 
